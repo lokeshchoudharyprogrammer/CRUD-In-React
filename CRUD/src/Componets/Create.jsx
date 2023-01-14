417ccd269e975cc0cd5beabb01f6d1e46a2e1601
Lokesh Choudhary <lokeshchoudhary2026@gmail.com>
2023-01-14 16:50:36 +0530

    CRUD-In-Reac t

--- CRUD/src/Componets/Create.jsx
+++ CRUD/src/Componets/Create.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { useState } from "react";
 import axios from "axios";
 import { useNavigate, Link } from "react-router-dom";
-
+// Create New Data
 const Create = () => {
   const [name, setname] = useState("");
   const [email, setemail] = useState("");
